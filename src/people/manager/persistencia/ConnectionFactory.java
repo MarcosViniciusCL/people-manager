ac147a522b93a2a8a70e5de8aa1e6d3c4068c16c
marcos <marcos@Turing>
2016-12-28 17:46:04 -0200

    Adicionado metodo de criar banco.

--- src/people/manager/persistencia/ConnectionFactory.java
+++ src/people/manager/persistencia/ConnectionFactory.java
@@ -5,6 +5,9 @@
  */
 package people.manager.persistencia;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +35,7 @@ public class ConnectionFactory {
             System.exit(0);
         }
         return null;
-        
+
     }
 
     public static void closeConnection(Connection con) {
@@ -69,6 +72,83 @@ public class ConnectionFactory {
     }
 
     public static void criarBanco() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection con = null;
+        try {
+            FileWriter arquivo = new FileWriter(new File("./database.db"));
+            arquivo.close();
+            con = getConnection();
+            Statement stmt;
+            stmt = con.createStatement();
+            String sql = "CREATE TABLE ATENDIMENTOS (ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "DATA_AGENDAMENTO TEXT, "
+                    + "DATA_ATENDIMENTO TEXT, "
+                    + "COMENTARIO TEXT, "
+                    + "ID_CLIENTE INTEGER, "
+                    + "ID_FUNCIONARIO INTEGER, "
+                    + "VALOR REAL, "
+                    + "ATENDIMENTO TEXT);";
+            stmt.executeUpdate(sql);
+            sql = "CREATE TABLE CLIENTES (ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "NOME TEXT, "
+                    + "ATIVO INTEGER, "
+                    + "SOBRENOME TEXT, "
+                    + "CELULAR TEXT, "
+                    + "TELEFONE TEXT, "
+                    + "EMAIL TEXT, "
+                    + "IDADE INTEGER, "
+                    + "NASCIMENTO TEXT, "
+                    + "CPF TEXT, "
+                    + "RG TEXT, "
+                    + "ENDERECO TEXT, "
+                    + "HISTORICO TEXT, "
+                    + "SALDODEVEDOR REAL, "
+                    + "ULTIMOATENDIMENTO TEXT, "
+                    + "PROXIMOATENDIMENTO TEXT);";
+            stmt.executeUpdate(sql);
+            sql = "CREATE TABLE PRODUTOS (ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "NOME TEXT, "
+                    + "CODIGOBARRA TEXT, "
+                    + "VALOR REAL, "
+                    + "QUANTIDADE INTEGER, "
+                    + "VALORCOMPRA REAL, "
+                    + "CATEGORIA TEXT);";
+            stmt.executeUpdate(sql);
+            sql = "CREATE TABLE USUARIOS (USUARIO TEXT, "
+                    + "SENHA TEXT, "
+                    + "NIVEL INTEGER);";
+            stmt.executeUpdate(sql);
+            sql = "CREATE TABLE VENDAS (ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "DATA_VENDA TEXT, "
+                    + "COMENTARIO TEXT, "
+                    + "ID_CLIENTE INTEGER, "
+                    + "ID_VENDEDOR INTEGER, "
+                    + "PRODUTOS TEXT, "
+                    + "VALOR REAL, "
+                    + "FORMA_PAGAMENTO TEXT, "
+                    + "VALOR_RECEBIDO REAL, "
+                    + "VALOR_TROCO REAL, "
+                    + "ESTADO TEXT);";
+            stmt.executeUpdate(sql);
+            sql = "CREATE TABLE VENDEDORES (ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "NOME TEXT, "
+                    + "ESTADO INTEGER, "
+                    + "SOBRENOME TEXT, "
+                    + "CELULAR TEXT, "
+                    + "EMAIL TEXT, "
+                    + "IDADE INTEGER, "
+                    + "NASCIMENTO TEXT, "
+                    + "DATA_CONTRATACAO TEXT, "
+                    + "CPF TEXT, "
+                    + "ENDERECO TEXT);";
+            stmt.executeUpdate(sql);
+            stmt.close();
+
+        } catch (IOException | SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if(con != null)
+                closeConnection(con);
+        }
+
     }
 }
